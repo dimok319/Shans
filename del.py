f883267adb6d88cef9f8a99e2c9ec062eeff6afc
dpyshnograev <dimok319@mail.ru>
2025-11-18 15:56:23 +0300

    fdd

---
+++ del.py
@@ -0,0 +1,9 @@
+words = ['абстракция', 'паттерн', 'программирование', 'веттер', 'ттт']
+patt = "тт"
+
+for word in words:
+    count = word.count(patt)
+    print(f"'слово '{word}' слово '{count}")
+
+    #
+    # Использование if for в списках
